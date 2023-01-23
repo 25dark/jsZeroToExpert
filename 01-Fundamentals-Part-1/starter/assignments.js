6b54693e6740a07a659c78707b15d0a94c51d3d5
25dark <henrydanso6@gmail.com>
2023-01-23 10:01:25 +0000

    added files

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+//JS Fundaments - Part 1
+//LECTURE: Values and Variables
+//Declare variables called 'country', 'continent',
+//'population' and assign their values according to your own country
+//log their values to the console
+
+let country = 'Ghana';
+let continent = 'Africa';
+let population = 100000;
+
+console.log(country + ' ' + 'is a country in ' + continent + ' ' + 'where we are about ' + population);

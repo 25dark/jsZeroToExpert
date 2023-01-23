1741d3f46fc1503c83f78b91e724b4cc46d95286
25dark <henrydanso6@gmail.com>
2023-01-23 12:20:31 +0000

    solved a challenge

---
+++ challenges/codingChallenge.js
@@ -0,0 +1,16 @@
+// Store Mark's and John's mass and height in variables
+let markMass = 78;
+let markHeight = 1.69;
+// Calculate both mark's BMIs using the formula
+
+console.log(markHigherBMI = markMass / (markHeight * markHeight));
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+console.log(johnHigherBMI = johnMass / (johnHeight * johnHeight));
+
+// Create a Boolean Variable 'markHigherBMI' containing info about whether
+// Mark has a high BMI than John
+
+console.log(markHigherBMI > johnHigherBMI);

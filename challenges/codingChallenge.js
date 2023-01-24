58c27eede64f07379c26239e2d4dd3e1961d0c37
25dark <henrydanso6@gmail.com>
2023-01-24 11:02:02 +0000

    added a challenge

--- challenges/codingChallenge.js
+++ challenges/codingChallenge.js
@@ -1,16 +1,50 @@
+//CODING CHALLENG
+
 // Store Mark's and John's mass and height in variables
-let markMass = 78;
-let markHeight = 1.69;
+const markMass = 78;
+const markHeight = 1.69;
+const markMass2 = 95;
+const markHeight2 = 1.88;
+
+
+const johnMass = 92;
+const johnHeight = 1.95;
+const johnMass2 = 85;
+const johnHeight2 = 1.76;
 // Calculate both mark's BMIs using the formula
+const BMIMark = markMass / markHeight ** 2;
+const BMIJohn = johnMass / (johnHeight * johnHeight);
 
-console.log(markHigherBMI = markMass / (markHeight * markHeight));
-
-let johnMass = 92;
-let johnHeight = 1.95;
-
-console.log(johnHigherBMI = johnMass / (johnHeight * johnHeight));
+const BMIMark2 = markMass2 / markHeight2 ** 2;
+const BMIJohn2 = johnMass2 / (johnHeight2 * johnHeight2);
+const markHeightBMI = BMIMark > BMIJohn;
 
 // Create a Boolean Variable 'markHigherBMI' containing info about whether
 // Mark has a high BMI than John
 
-console.log(markHigherBMI > johnHigherBMI);
+console.log(BMIMark, BMIJohn, markHeightBMI);
+
+
+
+
+
+// coding challenge 2
+// Use the BMI example from challenge #1, and the code you already wrote, and improve it
+
+/* print a nice output to the console, saying who has the higher BMI
+    Mark's BMI is higher than John's! or John's BMI is higher than Mark's!
+*/
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI is higher that John's`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+}
+
+// String Iteral
+
+if (BMIMark2 > BMIJohn2){
+    console.log(`Mark's BMI (${BMIMark2}) is higher than John's BMI (${BMIJohn2})`);
+} else {
+    console.log(`Mark's BMI (${BMIMark2}) is lesser than John's BMI (${BMIJohn2})`);
+}

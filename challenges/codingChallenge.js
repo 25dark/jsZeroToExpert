99561b0d8ba2af7ef641141a51f51be5131426e7
25dark <henrydanso6@gmail.com>
2023-01-25 15:24:02 +0000

    added a challenge

--- challenges/codingChallenge.js
+++ challenges/codingChallenge.js
@@ -25,9 +25,6 @@ const markHeightBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn, markHeightBMI);
 
 
-
-
-
 // coding challenge 2
 // Use the BMI example from challenge #1, and the code you already wrote, and improve it
 
@@ -47,4 +44,92 @@ if (BMIMark2 > BMIJohn2){
     console.log(`Mark's BMI (${BMIMark2}) is higher than John's BMI (${BMIJohn2})`);
 } else {
     console.log(`Mark's BMI (${BMIMark2}) is lesser than John's BMI (${BMIJohn2})`);
+}
+
+
+/* Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+//DATA 1 FOR DOLPHINS
+// const dolphinsScore1 = 96; 
+// const dolphinsScore2 = 108;
+// const dolphinsScore3 = 89;
+
+//DATA BONUS 1 FOR DOLPHINS
+// const dolphinsScore1 = 97; 
+// const dolphinsScore2 = 112;
+// const dolphinsScore3 = 101;
+
+//DATA BONUS 2 FOR DOLPHINS
+const dolphinsScore1 = 97;
+const dolphinsScore2 = 112;
+const dolphinsScore3 = 101;
+
+
+//DATA 1 FOR KOALAS
+// const koalasScore1 = 88;
+// const koalasScore2 = 91;
+// const koalasScore3 = 110;
+
+
+//DATA BONUS 1 FOR KOALAS
+// const koalasScore1 = 109;
+// const koalasScore2 = 95;
+// const koalasScore3 = 123;
+
+
+////DATA BONUS 2 FOR KOALAS
+const koalasScore1 = 109;
+const koalasScore2 = 95;
+const koalasScore3 = 106;
+
+//1
+const dolphinsAverageScore = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
+const koalasAverageScore = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
+
+console.log(dolphinsAverageScore, koalasAverageScore);
+
+//2
+if (dolphinsAverageScore > koalasAverageScore) {
+    console.log(`Dolphins with the average score of (${dolphinsAverageScore}) beats Koalas with a margin of (${dolphinsAverageScore - koalasAverageScore})`);
+} else if (dolphinsAverageScore === koalasAverageScore) {
+    console.log(`That was a tough competition because no one came first`);
+} else {
+    console.log(`Dolphins with the average score of (${dolphinsAverageScore}) couldn't beat Koalas average score of (${koalasAverageScore})`);
+}
+
+// BONUS Q1
+const minimumScore = 100;
+/*Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks */
+
+if (dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore >= minimumScore){
+    console.log(`Dolphins are the greatest of all time`);
+} else if (dolphinsAverageScore > koalasAverageScore && dolphinsAverageScore != minimumScore){
+    console.log(`We will be waiting for you guys next season`);
+}else if (dolphinsAverageScore === koalasAverageScore){
+    console.log(`We will be waiting for you two next season`);
+} else {
+    console.log(`Give a BIG shouts to the KOALAS for becoming the GOAT champions`);
 }

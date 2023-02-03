75bceaf5096b4b980ebd429a082771f4bfd753da
25dark <henrydanso6@gmail.com>
2023-02-03 01:52:31 +0000

    modified

--- challenges/codingChallenge.js
+++ challenges/codingChallenge.js
@@ -186,42 +186,79 @@ Apply this to the team's average scores �
 GOOD LUCK  */
 
 
-//DATA 1 FOR DOLPHINS
-const dolphinsScore1 = 44; 
-const dolphinsScore2 = 23;
-const dolphinsScore3 = 71;
+// //DATA 1 FOR DOLPHINS
+// const dolphinsScore1 = 44; 
+// const dolphinsScore2 = 23;
+// const dolphinsScore3 = 71;
 
 
-//DATA 1 FOR KOALAS
-const koalasScore1 = 65;
-const koalasScore2 = 54;
-const koalasScore3 = 49;
+// //DATA 1 FOR KOALAS
+// const koalasScore1 = 65;
+// const koalasScore2 = 54;
+// const koalasScore3 = 49;
 
 // Create an arrow function 'calcAverage' to calculate the average of 3 scores
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
-const dolphinsAverage = calcAverage(dolphinsScore1, dolphinsScore2, dolphinsScore3);
-console.log("dolphinsAverage", dolphinsAverage);
-
-
-const koalasAverage = calcAverage(koalasScore1, koalasScore2, koalasScore3);
-console.log("koalasAverage", koalasAverage);
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// average score of Dolhins
+// const avgDolhins = calcAverage(dolphinsScore1, dolphinsScore2, dolphinsScore3);
+// console.log(avgDolhins);
+// average score of Koalas
+// const avgKoalas = calcAverage(koalasScore1, koalasScore2, koalasScore3);
+// console.log(avgKoalas);
 
 // Create a function 'checkWinner' that takes the average score of each team
 // as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
 // to the console, together with the victory points, according to the rule above.
 // Example: "Koalas win (30 vs. 13)"
+// const checkWinner = (avgDolhins, avgKoalas) => {
+//     if (avgDolhins >= 2 * avgKoalas) {
+//         console.log(`Dolhins win (${avgDolhins} vs. ${avgKoalas})`);
+//     } else if (avgKoalas >= 2 * avgDolhins) {
+//         console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+//     } else {
+//         console.log(`no team wins!`);
+//     }
+// }
+// checkWinner(avgDolhins, avgKoalas);
 
-const checkWinner = (avgDolhins, avgKoalas) => {
+//ARRAY
+// const friends = ['Michael', 'Steven', 'Peter'];
+// const years = new Array(1991,1984, 2008, 2020);
 
-    if(avgDolhins >= 2 * avgKoalas){
-        return `Dolphins win (${avgDolhins} vs. ${avgKoalas})`;
-    } else if(avgKoalas >= 2 * avgDolhins){
-        return `Koalas win (${avgKoalas} vs. ${avgDolhins})`;
-    } else{
-        return `no team wins!`;
-    }
+// years[0] = 2030;
+// console.log(years);
+// console.log(friends[friends.length - 1]);
+// console.log(years[years.length - 1]);
 
+
+/* Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array) */
+
+
+
+const calcTip = (bill) => {
+    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+    return tip;
 }
 
-const winner = checkWinner(dolphinsAverage, koalasAverage);
-console.log(winner);
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log(bills, tips);

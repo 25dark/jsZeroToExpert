14cedf198ab0f33058702df20b489a67d47632c3
25dark <henrydanso6@gmail.com>
2023-02-07 16:21:46 +0000

    added a challenge

--- challenges/codingChallenge.js
+++ challenges/codingChallenge.js
@@ -240,10 +240,13 @@ Your tasks:
 the corresponding tip, calculated based on the rules above (you can check out
 the code from first tip calculator challenge if you need to). Use the function
 type you like the most. Test the function using a bill value of 100
+
 2. And now let's use arrays! So create an array 'bills' containing the test data
 below
+
 3. Create an array 'tips' containing the tip value for each bill, calculated from
 the function you created before
+
 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
 Test data: 125, 555 and 44
 Hint: Remember that an array needs a value in each position, and that value can
@@ -252,13 +255,22 @@ values (so don't store the tip values in separate variables first, but right in 
 array) */
 
 
-
-const calcTip = (bill) => {
-    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-    return tip;
+function calcTip (bill) {
+    return  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
+const calcTotal = calcTip(100);
+console.log(calcTotal);
 
 const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-console.log(bills, tips);
+function billLoop (bills) {
+    let newArray = [];
+    for(let i = 0; i < bills.length; i++){
+        
+         newArray.push(calcTip(bills[i]));
+        
+    }
+    return newArray;
+}
+const tips = billLoop(bills);
+console.log(tips);

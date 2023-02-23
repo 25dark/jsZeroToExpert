d87ad9089c2ad7175d15f597c114c247d772cd34
25dark <henrydanso6@gmail.com>
2023-02-23 15:05:49 +0000

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,5 +6,20 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', function () {
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+
+    btnCloseModal.addEventListener('click', function () {
+      modal.style.display = 'none';
+      overlay.style.display = 'none';
+      console.log('closed clicked');
+    });
+
+    overlay.addEventListener('click', function () {
+      modal.style.display = 'none';
+      overlay.style.display = 'none';
+      console.log('overlay clicked');
+    });
+  });
 }

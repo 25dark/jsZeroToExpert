9ec0ba400e584aa21426d4edf655ca87fa01d04c
25dark <henrydanso6@gmail.com>
2023-02-28 11:31:29 +0000

    eventListeners added

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,21 +5,33 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
+const closeModal = function () {
+  // modal.style.display = 'none';
+  // overlay.style.display = 'none';
+  // console.log('closed clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  // modal.style.display = 'block';
+  // overlay.style.display = 'block';
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    modal.style.display = 'block';
-    overlay.style.display = 'block';
-
-    btnCloseModal.addEventListener('click', function () {
-      modal.style.display = 'none';
-      overlay.style.display = 'none';
-      console.log('closed clicked');
-    });
-
-    overlay.addEventListener('click', function () {
-      modal.style.display = 'none';
-      overlay.style.display = 'none';
-      console.log('overlay clicked');
-    });
-  });
+  btnOpenModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 }
+
+// adding keydow event
+document.addEventListener('keydown', function (e) {
+  // when key pressed is ESC
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    // check if modal is On
+    // if modal is On, close it
+    closeModal();
+  }
+});

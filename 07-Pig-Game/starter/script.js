eb9bae00a87130dfb78243bd908148b2d7388620
25dark <henrydanso6@gmail.com>
2023-03-01 21:52:39 +0000

    functions

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,9 +1,38 @@
 'use strict';
-
+// selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+// starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let curentScore = 0;
+let activePlayer = 0;
+// Rolling dice functionality (click event)
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. check for rolled 1: if true,
+  if (dice !== 1) {
+    curentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      curentScore;
+  } else {
+    // switch to player 2
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});

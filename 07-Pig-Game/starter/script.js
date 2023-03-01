d94b1d7cc6f4020a066ba3756831d10a405042a7
25dark <henrydanso6@gmail.com>
2023-03-01 22:29:27 +0000

    debugging

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 let current0El = document.getElementById('current--0');
@@ -15,8 +17,17 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 const scores = [0, 0];
-let curentScore = 0;
+let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 // Rolling dice functionality (click event)
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -28,11 +39,25 @@ btnRoll.addEventListener('click', function () {
 
   // 3. check for rolled 1: if true,
   if (dice !== 1) {
-    curentScore += dice;
+    currentScore += dice;
     document.getElementById(`current--${activePlayer}`).textContent =
-      curentScore;
+      currentScore;
   } else {
     // switch to player 2
-    activePlayer = activePlayer === 0 ? 1 : 0;
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. add current score to the active players score
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. check if the player's score is >= 100
+  //finish the game
+
+  // switch to the next player
+  switchPlayer();
 });

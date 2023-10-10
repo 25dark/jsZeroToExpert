5bdea82f79d3fd6805dca5715b2cf75c3a56dfde
25Dark <henrydanso6@gmail.com>
2023-10-10 02:24:16 +0000

    js practising

---
+++ pratice.js
@@ -0,0 +1,13 @@
+//for each
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.forEach(function (el) {
+  if (el % 2 === 0) {
+    console.log(el);
+  }
+});
+
+// for (let el of numbers) {
+//   console.log(el);
+// }

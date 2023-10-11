4bd9fbbcae4b39b0e653bad41edc31bf3cc34303
25Dark <henrydanso6@gmail.com>
2023-10-11 03:53:23 +0000

    today's commit

--- pratice.js
+++ pratice.js
@@ -1,13 +1,44 @@
 //for each
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-numbers.forEach(function (el) {
-  if (el % 2 === 0) {
-    console.log(el);
-  }
-});
+// numbers.forEach(function (el) {
+//   if (el % 2 === 0) {
+//     console.log(el);
+//   }
+// });
 
 // for (let el of numbers) {
 //   console.log(el);
 // }
+
+// const movies = [
+//   { title: "The Shawshank Redemption", rating: 80 },
+//   { title: "Pulp Fiction", rating: 88 },
+//   { title: "Inception", rating: 90 },
+//   { title: "The Dark Knight", rating: 85 },
+//   { title: "Forrest Gump", rating: 92 },
+//   { title: "The Matrix", rating: 86 },
+//   { title: "Gladiator", rating: 87 },
+//   { title: "The Godfather", rating: 94 },
+//   { title: "Jurassic Park", rating: 82 },
+//   { title: "Avatar", rating: 89 },
+// ];
+
+// movies.forEach(function (movie) {
+//   console.log(`${movie.title} - ${movie.rating}/100`);
+// });
+
+// const tit = movies.forEach(function (t) {
+//   return t.title.toUpperCase();
+// });
+
+// arrow functions
+
+// Use the map method to create a new array of formatted movie titles and ratings
+// const formattedMovies = movies.map(
+//   (movie) => `${movie.title} - ${movie.rating / 10}`
+// );
+
+// // Log the new array
+// console.log(formattedMovies);

fd0830e72f0c59fa0ab27164a5c99bca8217b6ce
25Dark <henrydanso6@gmail.com>
2023-10-24 02:40:02 +0000

    updates

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,16 +1,29 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-// const container = document.querySelector("#container");
-// const baseURL =
-//   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const container = document.querySelector("#container");
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 //How I did mine
 
-const container = document.querySelector("#container");
+// const container = document.querySelector("#container");
 
-for (let i = 1; i <= 100; i++) {
+// for (let i = 1; i <= 100; i++) {
+//   const newImg = document.createElement("img");
+//   newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+
+//   container.appendChild(newImg);
+// }
+
+for (let i = 1; i <= 151; i++) {
+  const pokemon = document.createElement("div");
+  const label = document.createElement("span");
+  pokemon.classList.add("pokemon");
+  label.innerText = `#${i}`;
   const newImg = document.createElement("img");
-  newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+  newImg.src = `${baseURL}${i}.png`;
 
-  container.appendChild(newImg);
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
 }

09988e70277d24bdddea4cfe2c184da3aefc9605
25Dark <henrydanso6@gmail.com>
2023-10-28 17:05:45 +0000

    json

---
+++ JSON/app.js
@@ -0,0 +1,11 @@
+fetch("https://swapi.dev/api/people/1")
+  .then((res) => {
+    console.log("RESOLVED!", res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) => {
+    console.log("ERROR!", e);
+  });

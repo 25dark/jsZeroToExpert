8c42ea0c25d429dd25ec0388658136675dac9b17
25Dark <henrydanso6@gmail.com>
2023-10-30 20:27:57 +0000

    updates

--- JSON/app.js
+++ JSON/app.js
@@ -1,11 +1,30 @@
-fetch("https://swapi.dev/api/people/1")
-  .then((res) => {
-    console.log("RESOLVED!", res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.log("ERROR!", e);
-  });
+// fetch("https://swapi.dev/api/people/1")
+//   .then((res) => {
+//     console.log("RESOLVED!", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
+
+const jokeText = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("li");
+  newLI.append(jokeText);
+  jokes.append;
+};
+
+const getDadJoke = async () => {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/");
+    return res.data.joke;
+  } catch (e) {
+    return "No Joke Available! Sorry :(";
+  }
+};
+
+button.addEventListener("click", addNewJoke);

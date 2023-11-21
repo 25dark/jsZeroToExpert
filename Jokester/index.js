4c64f6a525b8e2bc49c136fa9c3c8fdb078f6918
25Dark <henrydanso6@gmail.com>
2023-11-21 18:09:21 +0000

    fixed bug

--- Jokester/index.js
+++ Jokester/index.js
@@ -1,6 +1,6 @@
 const jokes = require("give-me-a-joke");
 // To get a random dad joke
-giveMeAJoke.getRandomDadJoke(function (joke) {
+jokes.getRandomDadJoke(function (joke) {
   //=> console.log(joke);
-  console.dir(jokes);
+  console.log(joke);
 });

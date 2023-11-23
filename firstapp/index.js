3a2dcbff923719c3550a9a3c89a3fe5f66845143
25Dark <henrydanso6@gmail.com>
2023-11-23 08:35:09 +0000

    cat request

--- firstapp/index.js
+++ firstapp/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-  console.log("WE GOT A NEW REQUEST");
-  res.send("<h1>This is my header</h1>");
-});
+// app.use((req, res) => {
+//   console.log("WE GOT A NEW REQUEST");
+//   res.send("<h1>This is my header</h1>");
+// });
 
 app.get("/cats", (req, res) => {
   console.log("cat request");
 });
 // /cats => 'meow'
-// /dogs
+// /dogs => 'woof'
+// '/'
 
 app.listen(3000, () => {
   console.log("LISTENING ON PORT 3000");

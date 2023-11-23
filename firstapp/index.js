13871ff074f4d34c8f6eb5f92244a58efe2ef323
25Dark <henrydanso6@gmail.com>
2023-11-23 08:40:04 +0000

    routes

--- firstapp/index.js
+++ firstapp/index.js
@@ -6,8 +6,16 @@ const app = express();
 //   res.send("<h1>This is my header</h1>");
 // });
 
+app.get("/", (req, res) => {
+  app.send("<h1>This is the homepage</h1>");
+});
+
+app.get("/dogs", (req, res) => {
+  app.send("Dog page");
+});
+
 app.get("/cats", (req, res) => {
-  console.log("cat request");
+  app.send("cat page");
 });
 // /cats => 'meow'
 // /dogs => 'woof'

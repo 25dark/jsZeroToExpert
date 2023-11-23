e124b13f8dca6bf2b09f17864a24ebc31d646f91
25Dark <henrydanso6@gmail.com>
2023-11-23 02:49:39 +0000

    req res

--- firstapp/index.js
+++ firstapp/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use(() => {
+app.use((req, res) => {
   console.log("WE GOT A NEW REQUEST");
+  console.log("HELLO WE GOT YOUR REQUEST, THIS IS A RESPONSE");
 });
 
 app.listen(3000, () => {
